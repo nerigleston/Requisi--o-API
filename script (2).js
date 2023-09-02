b6818b4897485a569317b442faa5902103396bc0
Neri Filho <nerifilho1256@gmail.com>
2023-09-02 16:26:23 -0300

    Requisição API

--- script (2).js
+++ script (2).js
@@ -1,11 +1,18 @@
-const botaoFetch = window.document.getElementById("botaoFetch")
-const resultado = document.getElementById("resultado")
+document.addEventListener("DOMContentLoaded", () => {
+  const botaoFetch = document.getElementById("botaoFetch");
+  const resultado = document.getElementById("resultado");
 
-botaoFetch.addEventListener('click', ()=> {
-  fetch("https://dog.ceo/api/breed/corgi/images/random/3")
-  .then((res) => res.json())
-  .then((data) => data.message.map((foto)=>{
-    resultado.innerHTML += `<img src=${foto}/>`
-  }))
-  .catch(error => resultado.innerHTML = '<h1>Erro no fetch</h1>')
-})
+  botaoFetch.addEventListener('click', () => {
+    resultado.innerHTML = '';
+
+    fetch("https://dog.ceo/api/breed/corgi/images/random/3")
+      .then((res) => res.json())
+      .then((data) => {
+        data.message.forEach((foto) => {
+          resultado.innerHTML += `<img src="${foto}" alt="Corgi"/>`;
+        });
+      })
+      .catch(error => resultado.innerHTML = '<h1>Erro no fetch</h1>');
+  });
+});
+
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const botaoFetch = window.document.getElementById("botaoFetch")
-const resultado = document.getElementById("resultado")
+document.addEventListener("DOMContentLoaded", () => {
+  const botaoFetch = document.getElementById("botaoFetch");
+  const resultado = document.getElementById("resultado");
 
-botaoFetch.addEventListener('click', ()=> {
-  fetch("https://dog.ceo/api/breeds/image/random")
-  .then((res) => res.json())
-  .then((data) => resultado.innerHTML = `<img src=${data.message}/>`)
-  .catch((error) => console.log(error))
-})
+  botaoFetch.addEventListener('click', () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => resultado.innerHTML = `<img src="${data.message}" alt="Dog"/>`)
+      .catch((error) => console.log(error));
+  });
+});
