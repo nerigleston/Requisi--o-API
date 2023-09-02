b6df7becf6dbe60cc853e9ea463fc54dcf5bbff4
Neri Filho <nerifilho1256@gmail.com>
2023-09-02 16:18:33 -0300

    Calculadora

--- script (2).js
+++ script (2).js
@@ -3,8 +3,8 @@ const resultado = document.getElementById("resultado")
 
 botaoFetch.addEventListener('click', ()=> {
   fetch("https://dog.ceo/api/breed/corgi/images/random/3")
-  .then(res => {return res.json()})
-  .then(data => data.message.map((foto)=>{
+  .then((res) => res.json())
+  .then((data) => data.message.map((foto)=>{
     resultado.innerHTML += `<img src=${foto}/>`
   }))
   .catch(error => resultado.innerHTML = '<h1>Erro no fetch</h1>')
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const resultado = document.getElementById("resultado")
 
 botaoFetch.addEventListener('click', ()=> {
   fetch("https://dog.ceo/api/breeds/image/random")
-  .then(res => {return res.json()})
-  .then(data => resultado.innerHTML = `<img src=${data.message}/>`)
-  .catch(error => console.log(error))
+  .then((res) => res.json())
+  .then((data) => resultado.innerHTML = `<img src=${data.message}/>`)
+  .catch((error) => console.log(error))
 })
